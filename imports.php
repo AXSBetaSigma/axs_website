cdd23857b259f1530cc99638e9d7813898f901a4
JeDeveloper <joshuaevanslowell@gmail.com>
2019-02-12 17:42:47 -0500

    redid org structure. still recovering from my fling with require.js. NEVER FUCKING AGAIN

--- imports.php
+++ imports.php
@@ -1,10 +1,9 @@
 <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
-<link rel='stylesheet' href='css/bootstrap.css' type='text/css'>
+<link rel='stylesheet' href='plugins/bootstrap/css/bootstrap.css' type='text/css'>
 <link rel='stylesheet' href='css/style.css' type='text/css'>
 <link href="https://fonts.googleapis.com/css?family=Cagliostro|Domine" rel="stylesheet">
 <meta content="width=device-width, initial-scale=1" name="viewport" />
-<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
-<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
-<script src="https://d3js.org/d3.v4.min.js"></script>
+<script type="text/javascript" src="plugins/jquery/jquery.min.js"></script>
+<script type="text/javascript" src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="plugins/d3/d3.min.js"></script>
 <script type="text/javascript" src="js/scripts.js"></script>
-<script type="text/javascript" src="https://unpkg.com/popper.js/dist/umd/popper.min.js"></script>
